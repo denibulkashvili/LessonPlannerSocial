58b406a903d6a619d307424a64682e30329c412b
denibulkashvili <denibulkashvili@gmail.com>
2019-05-04 12:26:04 +0700

    Edit templates and static settings

--- lesson_planner/settings.py
+++ lesson_planner/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
 
 import environ
 env = environ.Env(
@@ -132,3 +133,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIR = [os.path.join(BASE_DIR, "static")]
