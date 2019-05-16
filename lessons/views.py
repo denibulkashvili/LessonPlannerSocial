d8fbda87c93ede947200eeaf431fbe95f5e1a7fd
denibulkashvili <denibulkashvili@gmail.com>
2019-05-17 00:34:51 +0700

    Add basic lesson views

--- lessons/views.py
+++ lessons/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.views.generic import CreateView, DetailView, ListView
+from lessons.models import Book, Lesson, Tag
 
 # Create your views here.
+class CreateLessonView(LoginRequiredMixin, CreateView):
+    fields = ('title', 'tags', 'book', 'lesson_number', 'lesson_duration',
+                'lesson_objectives', 'resources', 'content', 'video_url')    
+    model = Lesson       
+
+class LessonListView(ListView):
+    model = Lesson
+    context_object_name = "lesson_list"
+
+class LessonListByUser(ListView):
+    model = Lesson
+    context_object_name = "lesson_list"
+
+class LessonDetailView(DetailView):
+    model = Lesson
