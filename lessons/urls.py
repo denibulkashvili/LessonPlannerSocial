c4e6dc9bce86eddd0cce122dbcdf931448153f67
denibulkashvili <denibulkashvili@gmail.com>
2019-05-20 00:05:15 +0700

    Add new lessons urlconfs

--- lessons/urls.py
+++ lessons/urls.py
@@ -7,9 +7,6 @@ urlpatterns = [
   path("all/", views.LessonListView.as_view(), name="lesson_list"), 
   path("id/<pk>/", views.LessonDetailView.as_view(), name="lesson_detail"),
   path("create/", views.CreateLessonView.as_view(), name="create"),
-  # path("by_user/<username:username>/", views.LessonListByUser.as_view(), name="by_user"),
-  path("tags/", views.TagListView.as_view(), name="tag_list"),
-  path("tag/<slug:slug>", views.TagDetailView.as_view(), name="tag_detail"),
-  path("books/", views.BookListView.as_view(), name="book_list"),
-  path("book/<slug:slug>", views.BookDetailView.as_view(), name="book_detail"),
+  path("delete/<pk>/", views.DeleteLessonView.as_view(), name="delete"),
+  path("by/<username>/", views.LessonListByUser.as_view(), name="by_user"),
 ]
