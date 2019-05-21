8692469e65f714632d5113e3f85e8f3eeb33f77b
denibulkashvili <denibulkashvili@gmail.com>
2019-05-22 01:35:25 +0700

    Update static files setting

--- lesson_planner/settings.py
+++ lesson_planner/settings.py
@@ -134,8 +134,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRs = [os.path.join(BASE_DIR, "static")]
 
 
 LOGIN_REDIRECT_URL = "hello"
 LOGOUT_REDIRECT_URL = "goodbye"
+
