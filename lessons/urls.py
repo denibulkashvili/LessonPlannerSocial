78c02919ac05fc6852c7004f1d81e2b35f8cbdc8
denibulkashvili <denibulkashvili@gmail.com>
2019-05-22 01:37:50 +0700

    Add urlconf for books and tags

--- lessons/urls.py
+++ lessons/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
   path("create/", views.CreateLessonView.as_view(), name="create"),
   path("delete/<pk>/", views.DeleteLessonView.as_view(), name="delete"),
   path("by/<username>/", views.LessonListByUser.as_view(), name="by_user"),
+  path("with-tag/all/", views.TagListView.as_view(), name="tag_list"),
+  path("with-tag/<slug:slug>/", views.TagDetailView.as_view(), name="tag_detail"),
+  path("from-book/all/", views.BookListView.as_view(), name="book_list"),
+  path("from-book/<slug:slug>/", views.BookDetailView.as_view(), name="book_detail"),
 ]
