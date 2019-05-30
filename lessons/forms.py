578b4eb0ab9e24a81c307af37c4ad92f6cf19d7f
denibulkashvili <denibulkashvili@gmail.com>
2019-05-30 11:17:47 +0700

    Update previously edited files

--- lessons/forms.py
+++ lessons/forms.py
@@ -1,6 +1,5 @@
 from django import forms
 from .models import Lesson
-from mediumeditor.widgets import MediumEditorTextarea
 
 class CreateLessonForm(forms.ModelForm):
     """Form to create new lesson plans"""
@@ -18,10 +17,6 @@ class CreateLessonForm(forms.ModelForm):
             "content",
             "video_url",
         )
-        widgets = {
-            'content': MediumEditorTextarea(),
-            'lesson_objectives': MediumEditorTextarea(),
-            'resources': MediumEditorTextarea(),
-        }
+        
 
     
