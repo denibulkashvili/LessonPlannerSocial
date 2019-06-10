47c10507784bc9ae4fe8d2bc5f178d25889d30bf
denibulkashvili <denibulkashvili@gmail.com>
2019-06-10 14:36:46 +0700

    Add new update lesson

--- lessons/views.py
+++ lessons/views.py
@@ -149,6 +149,95 @@ class UpdateLessonView(UpdateView):
     template_name_suffix = "_update_form"
 
 
+class UpdateArrowLessonView(UpdateView):
+    """Update Arrow lesson view"""
+    
+    model = Lesson
+    fields = (
+        "title",
+        "tags",
+        "book",
+        "lesson_number",
+        "lesson_duration",
+        "lesson_objectives",
+        "resources",
+        "video_url",
+        # E-R-S-A-W
+        "engage_time",
+        "engage_description",
+        "review_time",
+        "review_description",
+        "study_time",
+        "study_description",
+        "activate_time",
+        "activate_description",
+        "wrap_up_time",
+        "wrap_up_description",
+    )
+    template_name_suffix = "_update_arrow_form"
+
+class UpdateBoomerangLessonView(UpdateView):
+    """Update Boomerang lesson view"""
+    
+    model = Lesson
+    fields = (
+         "title",
+        "tags",
+        "book",
+        "lesson_number",
+        "lesson_duration",
+        "lesson_objectives",
+        "resources",
+        "video_url",
+        # E-A-R-S-C-A2-W
+        "engage_time",
+        "engage_description",
+        "activate_time",
+        "activate_description",
+        "review_time",
+        "review_description",
+        "study_time",
+        "study_description",
+        "activate_time2",
+        "activate_description2",
+        "wrap_up_time",
+        "wrap_up_description",
+    )
+    template_name_suffix = "_update_boomerang_form"
+
+class UpdatePatchworkLessonView(UpdateView):
+    """Update Patchwork lesson view"""
+
+    model = Lesson
+    fields = (
+        "title",
+        "tags",
+        "book",
+        "lesson_number",
+        "lesson_duration",
+        "lesson_objectives",
+        "resources",
+        "video_url",
+        # E-R-A-A2-S-S2-E2-A3-W
+        "engage_time",
+        "engage_description",
+        "review_time",
+        "review_description",
+        "activate_time",
+        "activate_description",
+        "activate_time2",
+        "activate_description2",
+        "study_time",
+        "study_description",
+        "engage_time2",
+        "engage_description2",
+        "activate_time3",
+        "activate_description3",
+        "wrap_up_time",
+        "wrap_up_description",
+    )
+    template_name_suffix = "_update_patchwork_form"
+
 class TagListView(ListView):
     """Tag list view"""
 
