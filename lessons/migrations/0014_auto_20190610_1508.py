09a05ad14a49b565dbe7329764379d93d132c373
denibulkashvili <denibulkashvili@gmail.com>
2019-06-16 13:36:09 +0700

    pylint and black

--- lessons/migrations/0014_auto_20190610_1508.py
+++ lessons/migrations/0014_auto_20190610_1508.py
@@ -6,14 +6,18 @@ import django.db.models.deletion
 
 class Migration(migrations.Migration):
 
-    dependencies = [
-        ('lessons', '0013_auto_20190610_1507'),
-    ]
+    dependencies = [("lessons", "0013_auto_20190610_1507")]
 
     operations = [
         migrations.AlterField(
-            model_name='lesson',
-            name='book',
-            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, related_name='lessons', to='lessons.Book'),
-        ),
+            model_name="lesson",
+            name="book",
+            field=models.ForeignKey(
+                blank=True,
+                null=True,
+                on_delete=django.db.models.deletion.CASCADE,
+                related_name="lessons",
+                to="lessons.Book",
+            ),
+        )
     ]
